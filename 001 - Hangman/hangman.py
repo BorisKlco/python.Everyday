18e247f508bf9bd5750f132f5dc9bd2d6147259a
Boris Klco <klco@pm.me>
2023-02-14 20:24:04 +0000

    ClassesPog

--- 001 - Hangman/hangman.py
+++ 001 - Hangman/hangman.py
@@ -2,23 +2,35 @@ import requests
 import re
 
 
-def getWord():
-    ranWord = requests.get("https://random-word-api.herokuapp.com/word")
-    return ranWord.json()
+class Hangman:
+    def __init__(self, word):
+        self.word = word
+
+    def __str__(self):
+        return f"{self.word}"
+
+    @classmethod
+    def start(cls):
+        ranWord = requests.get("https://random-word-api.herokuapp.com/word")
+        word = ranWord.json()
+        return cls(word[0])
 
 
-while True:
-    test = []
-    word = getWord()
-    for letter in word[0]:
-        test.append(letter)
+test = Hangman.start()
+print(test)
 
-    # guess = input("Guess letter: ").lower()
-    # if len(guess) == 1:
-    #     if re.match(r"[a-z]", guess):
-    #         print(guess)
-    #         print(test)
-    #     else:
-    #         print("Only a-z")
-    # else:
-    #     print("Only one letter")
+# while True:
+#     test = []
+#     word = getWord()
+#     for letter in word[0]:
+#         print(letter)
+
+# guess = input("Guess letter: ").lower()
+# if len(guess) == 1:
+#     if re.match(r"[a-z]", guess):
+#         print(guess)
+#         print(test)
+#     else:
+#         print("Only a-z")
+# else:
+#     print("Only one letter")

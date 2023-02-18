f743addf81dbc42460c2a65870d627d57a0be73f
BorisKlco <klco@pm.me>
2023-02-18 18:18:08 +0100

    pokrok

--- 004 - Blackjack/bj.py
+++ 004 - Blackjack/bj.py
@@ -1,18 +1,30 @@
 import random
-import itertools as it
 
-
-deck = list(it.product("♠♣♥♦", [str(x) for x in range(2, 11)] + list("JQKA")))
-print(deck)
-random.shuffle(deck)
-print(deck)
-
-cards = [
-    (s, v)
-    for s in ["H", "S", "C", "D"]
+ccards = [
+    (v, s)
+    for s in ["♥", "♠", "♣", "♦"]
     for v in [str(i) for i in range(2, 11)] + list("JKQA")
 ]
 
-print(cards)
-random.shuffle(cards)
-print(cards)
+# print(cards)
+# random.shuffle(cards)
+# print(cards)
+
+
+class Blackjack:
+    def __init__(self) -> None:
+        self.cards = [
+            (v, s)
+            for s in ["♥", "♠", "♣", "♦"]
+            for v in [str(i) for i in range(2, 11)] + list("JKQA")
+        ]
+
+    def pull(self):
+        card = random.randint(1, 52)
+        print(self.cards[card])
+        random.shuffle(self.cards)
+        print(self.cards[card])
+
+
+test = Blackjack()
+test.pull()

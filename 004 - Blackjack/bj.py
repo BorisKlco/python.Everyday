d2bf490d9629ec44576bb7391f7c4b73731c185b
Boris Klco <klco@pm.me>
2023-02-20 11:54:21 +0000

    Trapenie😢

--- 004 - Blackjack/bj.py
+++ 004 - Blackjack/bj.py
@@ -29,8 +29,30 @@ class Blackjack:
             else:
                 value = [value[0] + int(face[0]), value[1] + int(face[0])]
         if value in ([21, 11], [11, 21]):
-            value = [21, 21]
+            value = ["Blackjack", 21]
         return value
+
+    def player_print(self):
+        print("Player: ", end="")
+        for _, face in enumerate(self.player["cards"]):
+            print(f"{face[0]}{face[1]} ", end="")
+        print(
+            "Score:",
+            self.score(self.player)[0]
+            if self.score(self.player)[0] == self.score(self.player)[1]
+            else self.score(self.player),
+        )
+
+    def dealer_print(self):
+        print("Delear: ", end="")
+        for _, face in enumerate(self.dealer["cards"]):
+            print(f"{face[0]}{face[1]} ", end="")
+        print(
+            "Score:",
+            self.score(self.dealer)[0]
+            if self.score(self.dealer)[0] == self.score(self.dealer)[1]
+            else self.score(self.dealer),
+        )
 
 
 game = Blackjack()
@@ -38,24 +60,43 @@ game = Blackjack()
 
 def start():
     print(f"Dealer: {game.dealer['cards'][0][0]}{game.dealer['cards'][0][1]} 🃏")
-    print("Player: ", end="")
-    for _, face in enumerate(game.player["cards"]):
-        print(f"{face[0]}{face[1]} ", end="")
-    print(
-        "Score:",
-        game.score(game.player)[0]
-        if game.score(game.player)[0] == game.score(game.player)[1]
-        else game.score(game.player),
-    )
-    if game.score(game.dealer) == [21, 21]:
-        sys.exit("Blackjack! Dealer won.")
-    elif game.score(game.player) == [21, 21]:
+    game.player_print()
+    print()
+    if game.score(game.player) == ["Blackjack", 21]:
         sys.exit("Blackjack! Player won.")
-    if input("Hit (y/n)").lower() == "y":
-        game.player["cards"].append(game.hit())
-        start()
-    else:
-        pass
+    elif game.score(game.dealer) == ["Blackjack", 21]:
+        print(
+            f"Dealer: {game.dealer['cards'][0][0]}{game.dealer['cards'][0][1]} {game.dealer['cards'][1][0]}{game.dealer['cards'][1][1]}"
+        )
+        sys.exit("Blackjack! Dealer won.")
+    question = input("Hit (y/n)? ")
+    if question.lower() == "y":
+        next_card = game.hit()
+        print(f"{next_card[0]}{next_card[1]} , sir!")
+        print()
+        game.player["cards"].append(next_card)
+        if game.score(game.player)[1] < 22:
+            start()
+        else:
+            game.player_print()
+            sys.exit("More than 21, sorry.")
+    elif question.lower() == "n":
+        # game.dealer_print()
+
+        # while game.score(game.dealer)[1] < game.score(game.player)[1]:
+        #     dealer_hit = game.hit()
+        #     print(f"Card for dealer: {dealer_hit[0]}{dealer_hit[1]}")
+        #     game.dealer["cards"].append(dealer_hit)
+
+        if game.score(game.dealer)[1] > 21:
+            game.dealer_print()
+            sys.exit("Dealer more than 21, Cg, you won!")
+        elif game.score(game.dealer)[1] < game.score(game.player)[1]:
+            game.player_print()
+            game.dealer_print()
+            sys.exit("Dealer won, sorry")
+
+        # sys.exit("More than 21, sorry.")
 
 
 while True:

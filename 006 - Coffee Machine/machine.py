73d4f4912114079eb0472cee33d88ba488f99645
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-02-22 22:02:53 +0100

    Oh boy 🙄

---
+++ 006 - Coffee Machine/machine.py
@@ -0,0 +1,15 @@
+"""
+resources 
+    300ml water
+    200ml milk
+    100g coffee
+3 drinks
+    Espresso - $1.50 - 50ml water, 18g coffee
+    Latte - $2.50 - 200ml water, 24g coffee, 150ml milk
+    Cappuccino - $3.00 - 250ml water, 24g coffee, 100ml milk
+Taking Coins
+    penny - 1c
+    Nickle - 5c
+    Dime - 10c
+    Quarter - 25c
+"""

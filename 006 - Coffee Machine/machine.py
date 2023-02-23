238dd985926fa8452fb282387ffb38b8b09d2072
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-02-23 21:50:58 +0100

    🙄

--- 006 - Coffee Machine/machine.py
+++ 006 - Coffee Machine/machine.py
@@ -1,8 +1,8 @@
 """
 resources 
-    300ml water
-    200ml milk
-    100g coffee
+    3000ml water
+    2000ml milk
+    200g coffee
 3 drinks
     Espresso - $1.50 - 50ml water, 18g coffee
     Latte - $2.50 - 200ml water, 24g coffee, 150ml milk
@@ -13,3 +13,41 @@ Taking Coins
     Dime - 10c
     Quarter - 25c
 """
+
+
+class Machine:
+    def __init__(self, ingredients, price):
+        self.ingredients = ingredients
+        self.price = price
+        self.water = 3000
+        self.milk = 2000
+        self.coffee = 200
+
+    def print(self):
+        print(self.ingredients, self.price)
+
+
+class Balance:
+    def deposit(self):
+        coins = [1, 5, 10, 25]
+
+
+class Coffee:
+    def espresso(self):
+        ingredients = {"water": 50, "milk": 0, "coffee": 18}
+        price = 1.5
+        return Machine(ingredients, price)
+
+    def latte(self):
+        ingredients = {"water": 200, "milk": 150, "coffee": 24}
+        price = 2.5
+        return Machine(ingredients, price)
+
+    def cappuccino(self):
+        ingredients = {"water": 250, "milk": 100, "coffee": 24}
+        price = 3
+        return Machine(ingredients, price)
+
+
+coffeeMachine = Coffee()
+coffeeMachine.latte().print()

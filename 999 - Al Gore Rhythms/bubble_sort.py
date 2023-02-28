1d3c955cdf64907c14ecb4e65120f43e1906b279
Boris Klco <klco@pm.me>
2023-02-28 10:56:01 +0000

    AlGore

--- 999 - Al Gore Rhythms/bubble_sort.py
+++ 999 - Al Gore Rhythms/bubble_sort.py
@@ -9,8 +9,8 @@ otherwise n +=1
 bubble = [2, 1, 7, 5, 6, 9, 8, 3, 4]
 print(bubble)
 
-for n in range(len(bubble) - 1):
-    for n in range(len(bubble) - 1):
+for i in range(len(bubble) - 1):
+    for n in range(len(bubble) - 1 - i):
         if bubble[n] > bubble[n + 1]:
             value_holder = bubble[n]
             bubble[n] = bubble[n + 1]

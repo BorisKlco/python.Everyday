f75b66a0afb6bd0a46fc8833158eabb5d75f8caa
Boris Klco <klco@pm.me>
2023-02-28 15:17:21 +0000

    Find minimum 🔍 in array.

--- 999 - Al Gore Rhythms/bubble_sort.py
+++ 999 - Al Gore Rhythms/bubble_sort.py
@@ -7,7 +7,7 @@ if bigger, than swap
 otherwise n +=1
 """
 bubble = [2, 1, 7, 5, 6, 9, 8, 3, 4]
-print(bubble)
+print("Bubble sort, usorted", bubble)
 
 for i in range(len(bubble) - 1):
     for n in range(len(bubble) - 1 - i):
@@ -17,4 +17,29 @@ for i in range(len(bubble) - 1):
             bubble[n + 1] = value_holder
 
 
-print(bubble)
+print("Bubble sort, sorted", bubble)
+
+print()
+# Challenge: Find minimum in subarray
+# https://www.khanacademy.org/computing/computer-science/algorithms/sorting-algorithms/pc/challenge-find-minimum-in-subarray
+finding_minimum_array = [18, 6, 66, 44, 9, 22, 14]
+
+
+def finding_minimum(array, position):
+    """
+    set min value and index,
+    iterate over array from provided position
+    if current value is lower than min value than swap min value for new one
+    """
+
+    min_value = array[position]
+    min_index = position
+    for num in range(min_index, len(array)):
+        if array[num] < min_value:
+            min_value = array[num]
+            min_index = num
+    return min_value, min_index
+
+
+print("Find min value in array from index of x", finding_minimum_array)
+print("Min value+Indexof from index of 2:", finding_minimum(finding_minimum_array, 2))

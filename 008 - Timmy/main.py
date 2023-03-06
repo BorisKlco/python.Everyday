6b1ce86e0550baed361e198e8ece8527f64bf694
Boris Klco <klco@pm.me>
2023-03-06 10:41:14 +0000

    Pen position reset

--- 008 - Timmy/main.py
+++ 008 - Timmy/main.py
@@ -1,9 +1,16 @@
+"""008 - Turtle"""
 from turtle import Screen, Turtle
 
 timmy = Turtle()
-
 screen = Screen()
 
+
+def resetPos():
+    timmy.setposition(0, 0)
+    timmy.pendown()
+
+
+"""Dash line"""
 # status = 1
 # def draw(status):
 #     if status == -1:
@@ -21,3 +28,13 @@ for _ in range(20):
     timmy.forward(10)
     timmy.penup()
     timmy.forward(10)
+
+resetPos()
+
+""" Triangle, square, pentagon, hex, hep, oct, mono and deca"""
+for n in range(3, 10):
+    for _ in range(n):
+        v = float(n)
+        timmy.pencolor(v / 15, v / 10, v / 12)
+        timmy.forward(50)
+        timmy.right(360 / n)

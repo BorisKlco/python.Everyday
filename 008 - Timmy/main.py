20b09abe6d8c7673ba43e12c452dbf11d06a92c7
Boris Klco <klco@pm.me>
2023-03-06 11:51:42 +0000

    +Random walk, fix draw_shapes

--- 008 - Timmy/main.py
+++ 008 - Timmy/main.py
@@ -1,43 +1,68 @@
 """008 - Turtle"""
+import random as r
 from turtle import Screen, Turtle
 
 
-def resetPos():
+def reset_pos(x_pos=-150, y_pos=150):
+    """Pen position reset"""
     timmy.penup()
-    timmy.setposition(-150, 150)
+    timmy.setposition(x_pos, y_pos)
     timmy.pendown()
 
 
 timmy = Turtle()
 screen = Screen()
-resetPos()
+reset_pos()
 
 
-"""Dash line"""
-# status = 1
-# def draw(status):
-#     if status == -1:
-#         timmy.pendown()
-#     else:
-#         timmy.penup()
-# for n in range(20):
-#     status = status * -1
-#     draw(status)
-#     timmy.forward(10)
+def dash_line():
+    """Draw a Dash line"""
+    # status = 1
+    # def draw(status):
+    #     if status == -1:
+    #         timmy.pendown()
+    #     else:
+    #         timmy.penup()
+    # for n in range(20):
+    #     status = status * -1
+    #     draw(status)
+    #     timmy.forward(10)
 
-# Actually not over-engineered dash line loop...
-for _ in range(20):
-    timmy.pendown()
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
+    # Actually not over-engineered dash line loop...
+    for _ in range(20):
+        timmy.pendown()
+        timmy.forward(10)
+        timmy.penup()
+        timmy.forward(10)
 
-resetPos()
 
-""" Triangle, square, pentagon, hex, hep, oct, mono and deca"""
-for n in range(3, 11):
-    for _ in range(n):
-        v = float(n)
-        timmy.pencolor(v / 15, v / 10, v / 12)
-        timmy.forward(100)
-        timmy.right(360 / n)
+reset_pos()
+dash_line()
+
+
+def draw_shapes():
+    """Draw a Triangle, square, pentagon, hex, hep, oct, mono and deca"""
+    for shape in range(3, 11):
+        timmy.pencolor(r.randint(0, 1), r.randint(0, 1), r.randint(0, 1))
+        timmy.pensize(3)
+        for _ in range(shape):
+            timmy.forward(100)
+            timmy.right(360 / shape)
+
+
+reset_pos()
+draw_shapes()
+
+
+def random_walk():
+    """Draw a Random Walk"""
+    turn = [270, 0, 90]
+    timmy.pensize(10)
+    timmy.pencolor(r.randint(0, 1), r.randint(0, 1), r.randint(0, 1))
+    timmy.right(r.choice(turn))
+    timmy.forward(25)
+
+
+reset_pos(0, 0)
+for _ in range(100):
+    random_walk()

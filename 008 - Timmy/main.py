4128dbfc9f93ac739c3c1721592e5205d68b405f
Boris Klco <klco@pm.me>
2023-03-06 08:29:02 +0000

    not over-engineered dash line loop...

--- 008 - Timmy/main.py
+++ 008 - Timmy/main.py
@@ -3,17 +3,21 @@ from turtle import Screen, Turtle
 timmy = Turtle()
 
 screen = Screen()
-status = 1
 
+# status = 1
+# def draw(status):
+#     if status == -1:
+#         timmy.pendown()
+#     else:
+#         timmy.penup()
+# for n in range(20):
+#     status = status * -1
+#     draw(status)
+#     timmy.forward(10)
 
-def draw(status):
-    if status == -1:
-        timmy.pendown()
-    else:
-        timmy.penup()
-
-
-for n in range(20):
-    status = status * -1
-    draw(status)
+# Actually not over-engineered dash line loop...
+for _ in range(20):
+    timmy.pendown()
+    timmy.forward(10)
+    timmy.penup()
     timmy.forward(10)

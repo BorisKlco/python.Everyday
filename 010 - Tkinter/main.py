344beb8f1e8e77518f239d2da0eb535c76216e0a
Boris Klco <klco@pm.me>
2023-03-23 22:25:47 +0100

    kinterMeSoftly

--- 010 - Tkinter/main.py
+++ 010 - Tkinter/main.py
@@ -7,4 +7,16 @@ window.minsize(width=500, height=300)
 label_text = tkinter.Label(text="Hello Window!", font=("Arial", 24))
 label_text.pack()
 
+
+def click_me():
+    label_text["text"] = userInput.get()
+
+
+button = tkinter.Button(text="Click me!", command=click_me)
+button.pack()
+
+userInput = tkinter.Entry()
+userInput.pack()
+
+
 window.mainloop()

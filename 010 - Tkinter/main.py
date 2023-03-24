01980664e27a34305d5edd8655e5a30195517edf
Boris Klco <klco@pm.me>
2023-03-24 13:33:01 +0100

    km to miles

--- 010 - Tkinter/main.py
+++ 010 - Tkinter/main.py
@@ -1,3 +1,4 @@
+import random
 import tkinter
 
 window = tkinter.Tk()
@@ -5,18 +6,24 @@ window.title("GUI <3")
 window.minsize(width=500, height=300)
 
 label_text = tkinter.Label(text="Hello Window!", font=("Arial", 24))
-label_text.pack()
+label_text.grid(column=0, row=0)
 
 
 def click_me():
-    label_text["text"] = userInput.get()
+    label_text["text"] = user_input.get()
 
 
-button = tkinter.Button(text="Click me!", command=click_me)
-button.pack()
+def click_random():
+    label_text["text"] = random.randint(1, 100)
 
-userInput = tkinter.Entry()
-userInput.pack()
 
+click_button = tkinter.Button(text="Click me!", command=click_me)
+click_button.grid(column=1, row=1)
+
+random_button = tkinter.Button(text="Random int", command=click_random)
+random_button.grid(column=2, row=0)
+
+user_input = tkinter.Entry()
+user_input.grid(column=3, row=2)
 
 window.mainloop()
---
+++ 010 - Tkinter/miles.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+window = Tk()
+window.title("km/miles calculator")
+window.minsize(width=400, height=200)
+window.config(padx=50, pady=50)
+
+
+def convert():
+    user_input = km_input.get()
+    try:
+        miles_convert["text"] = round(float(user_input) * 0.62, 1)
+    except:
+        miles_convert["text"] = "Not a number"
+
+
+km_text = Label(text="km", font=("Arial", 12))
+km_text.grid(column=2, row=0)
+
+km_input = Entry()
+km_input.grid(column=1, row=0)
+
+miles_text = Label(text="is equal to", font=("Arial", 12))
+miles_text.grid(column=0, row=1)
+
+miles_convert = Label(text="", font=("Arial", 12))
+miles_convert.grid(column=1, row=1)
+
+miles_km = Label(text=" miles", font=("Arial", 12))
+miles_km.grid(column=2, row=1)
+
+convert_button = Button(text="Convert", command=convert)
+convert_button.grid(column=1, row=2)
+
+
+window.mainloop()

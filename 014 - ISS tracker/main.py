9b0a926e81f994625ddc237cc3b460883dd99a7a
Boris Klco <klco@pm.me>
2023-04-02 20:23:24 +0200

    Watching iss

--- 014 - ISS tracker/main.py
+++ 014 - ISS tracker/main.py
@@ -1,10 +1,19 @@
 import json
 
+SLOVAKIA = (48.14816, 17.10674)
+
 import requests
 
 ISS_URL = "http://api.open-notify.org/iss-now.json"
 
+
 resp = requests.get(ISS_URL, timeout=5)
 data = resp.json()
 
-print(data)
+iss_lat = float(data["iss_position"]["latitude"])
+iss_long = float(data["iss_position"]["longitude"])
+
+if round(iss_lat) > 38 and round(iss_long) < 25:
+    print("ok")
+
+print(iss_long, iss_lat)
